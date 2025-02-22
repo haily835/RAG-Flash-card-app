e60ac70a4443b004a1f0695120d48f8606641564
Hai Ly <53532432+haily835@users.noreply.github.com>
2025-02-22 12:59:17 -0500

    Update ingest.py

--- src/ingest.py
+++ src/ingest.py
@@ -4,9 +4,10 @@ import numpy as np
 from langchain.document_loaders import PyPDFLoader, TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import pickle
-from chromadb import PersistentClient  # Import ChromaDB client
+from langchain_chroma import Chroma
 import uuid
-
+from langchain_openai import OpenAIEmbeddings
+import re
 
 
 def save_text(texts, vector_db_path="../models/embeddings/chroma_index"):
@@ -39,9 +40,12 @@ def load_documents(data_dir="./data"):
     return docs
 
 
-def get_embedders(model_name="sentence-transformers/all-MiniLM-L6-v2"):
+
+
+
+def get_embedder(model_name="text-embedding-3-small"):
     """Get the embedding function from sentence_transformers"""
-    return SentenceTransformer(model_name)
+    return OpenAIEmbeddings(model=model_name)
     
  
 def chunk_text(documents, chunk_size=1500, chunk_overlap=250):    
@@ -53,20 +57,57 @@ def chunk_text(documents, chunk_size=1500, chunk_overlap=250):
     
     return text_splitter.split_documents(documents)
 
+def load_vectorstore(file_name, embedding_function, vectorstore_path="db"):
 
-def create_chroma_index(db_path, texts):
+    """
+    Load a previously saved Chroma vector store from disk.
+
+    :param file_name: The name of the file to load (without the path)
+    :param api_key: The OpenAI API key used to create the vector store
+    :param vectorstore_path: The path to the directory where the vector store was saved (default: "db")
+    
+    :return: A Chroma vector store object
+    """
+    return Chroma(persist_directory=vectorstore_path, 
+                  embedding_function=embedding_function, 
+                  collection_name=clean_filename(file_name))
+
+
+def clean_filename(filename):
+    """
+    Remove "(number)" pattern from a filename 
+    (because this could cause error when used as collection name when creating Chroma database).
+
+    Parameters:
+        filename (str): The filename to clean
+
+    Returns:
+        str: The cleaned filename
+    """
+    # Regular expression to find "(number)" pattern
+    new_filename = re.sub(r'\s\(\d+\)', '', filename)
+    return new_filename
+
+def create_chroma_index(db_path, file_name, embedding_function , texts):
     """Create a ChromaDB index using the default embedding function"""
-    client = PersistentClient(path=db_path)
+  
+    
     unique_ids = set()
     unique_chunks = []
-    ids = [str(uuid.uuid5(uuid.NAMESPACE_DNS, t)) for t in texts]
+    ids = [str(uuid.uuid5(uuid.NAMESPACE_DNS, t.page_content)) for t in texts]
+
+
 
     for t, id in zip(texts, ids):     
-        if id not in unique_ids:       
+        if id not in unique_ids:
             unique_ids.add(id)
-            unique_chunks.append(t)     
-    
-    collection = client.create_collection("documents")  # Create a collection
-    collection.add(ids, documents=unique_chunks)  # Add embeddings to the collection
-    return collection
+            unique_chunks.append(t)
+    vector_store = Chroma.from_documents(
+        collection_name=clean_filename(file_name),
+        ids=list(unique_ids), 
+        documents=unique_chunks, 
+        embedding=embedding_function,
+        persist_directory=db_path,  # Where to save data locally, remove if not necessary
+    )
+    return vector_store
 
